045632b2ce2b8206e31507f46f4727b389e48566
EloyR <eloy.roger.araya@gmail.com>
2025-04-14 19:45:41 +0000

    faltaTerminar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let suitTops = ["♥", "♦", "♣", "♠"]; 
+  let suitButtoms = ["♥", "♦", "♣", "♠"];
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  
+  function generarCarta() {
+  let suitButtom = suitButtoms[Math.floor(Math.random() * suitButtoms.length)];
+  let suitTop = suitTops[Math.floor(Math.random() * suitTops.length)];
+  let value = values[Math.floor(Math.random() * values.length)];
+
+  let color = (suitTop === "♥" || suitTop === "♦") ? "rojo" : "negro";
+// document.querySelector("#carta").textContent = '♥'
+
+document.getElementById("carta").innerHTML = `
+<div class="suitTop ${color}">${suitTop}</div>
+<div class="value ${color}">${value}</div>
+<div class="suitButtom ${color}">${suitButtom}</div>
+`;
+}
+
+generarCarta(); 
 };
